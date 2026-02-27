25d9a0e19f54f90f5acf16f78680f1e3924d05b7
nasr <nsrddyn@gmail.com>
2026-02-27 17:37:51 +0100

    feature: improved lexing

--- source/editor/editor_lexer.h
+++ source/editor/editor_lexer.h
@@ -6,69 +6,103 @@ typedef struct token_node token_node;
 
 enum token_type
 {
- TokenUndefined = 256,
- TokenIdentifier,
- TokenIdentifierValue,
- TokenString,
- TokenNumber,
- TokenDoubleEqual,
- TokenGreaterEqual,
- TokenLesserEqual,
- TokenParam,
- TokenFunc,
- TokenReturn,
- TokenIf,
- TokenElse,
- TokenFor,
- TokenWhile,
- TokenBreak,
- TokenContinue,
- TokenExpression,
- TokenWhiteSpace,
- TokenComparisonParam,
- TokenFuncBody,
- TokenUnwantedChild,
- TokenNewLine,
- TokenRightShift,
- TokenLeftShift
+    TokenUndefined = 256,
+    TokenIdentifier,
+    TokenIdentifierAssignmentValue,
+    TokenValue,
+    TokenString,
+    TokenNumber,
+    TokenDoubleEqual,
+    TokenGreaterEqual,
+    TokenLesserEqual,
+    TokenParam,
+    TokenFunc,
+    TokenReturn,
+    TokenIf,
+    TokenElse,
+    TokenFor,
+    TokenWhile,
+    TokenBreak,
+    TokenContinue,
+    TokenExpression,
+    TokenWhiteSpace,
+    TokenComparisonParam,
+    TokenFuncBody,
+    TokenUnwantedChild,
+    TokenNewLine,
+    TokenRightShift,
+    TokenLeftShift
 };
 
 enum token_flags
 {
- FlagNone            = (0),
- FlagConstant        = (1 << 0),
- FlagGlobal          = (1 << 1),
- FlagsValue          = (1 << 2),
- FlagDefinition      = (1 << 3),
- FlagComparison      = (1 << 4),
- FlagTranslationUnit = (1 << 5),
- FlagDeprecated      = (1 << 6),
- FlagDirty           = (1 << 7),
+    FlagNone            = (0),
+    FlagConstant        = (1 << 0),
+    FlagGlobal          = (1 << 1),
+    FlagsValue          = (1 << 2),
+    FlagDefinition      = (1 << 3),
+    FlagComparison      = (1 << 4),
+    FlagTranslationUnit = (1 << 5),
+    FlagDeprecated      = (1 << 6),
+    FlagDirty           = (1 << 7),
 };
 
 struct token
 {
- str8        Lexeme;
- token_type  Type;
- token_flags Flags;
- u64         ByteOffset;
- s32         Column;
- s32         Line;
+    str8        Lexeme;
+    token_type  Type;
+    token_flags Flags;
+    u64         ByteOffset;
+    s32         Column;
+    s32         Line;
 };
 
 struct token_node
 {
- token_node *Next;
- token_node *Previous;
- u64         ParentHandle;
- u64         ChildHandle;
- token      *Token;
+    token_node *Next;
+    token_node *Previous;
+    u64         ParentHandle;
+    u64         ChildHandle;
+    token      *Token;
 };
 
 struct token_list
 {
- token_node *Root;
- token_node *Current;
+    token_node *Root;
+    token_node *Current;
+};
+
+global_variable const
+rune delimiters[]
+{
+        '{',
+        '}',
+        '(',
+        ')',
+        '[',
+        ']',
+        ';',
+};
+
+read_only global_variable
+token nil_token =
+{
+    .Lexeme     = {NULL, 0},
+    .Type       = TokenUndefined,
+    .Flags      = FlagNone,
+    .ByteOffset = 0,
+    .Column     = 0,
+    .Line       = 0,
+};
+
+read_only global_variable
+token_node nil_token_node =
+{
+    .Next         = &nil_token_node,
+    .Previous     = &nil_token_node,
+    .ParentHandle = 0,
+    .ChildHandle  = 0,
+    .Token        = 0,
 };
 
 #endif // EDITOR_LEXER_H
