bbb65cbf630bdaa6c4ef1633e59edb7adcb311d7
nasr <nsrddyn@gmail.com>
2026-03-07 13:54:21 +0100

    feature: enum for language types

---
+++ source/editor/editor_type_checking.h
@@ -0,0 +1,41 @@
+#ifndef TYPE_CHECKING_H
+#define TYPE_CHECKING_H
+
+/* NOTE(nasr): defining the primitve types */
+enum editor_types
+{
+
+  TypeFloat32,
+  TypeFloat64
+
+  TypeSInt8
+  TypeSInt16
+  TypeSInt32,
+  TypeSInt64,
+
+  TypeUInt8
+  TypeUInt16
+  TypeUInt32,
+  TypeUInt64,
+
+  TypeChar,
+
+  TypeVoid,
+
+  TypePointer,
+  TypeArray,
+  TypeEnum,
+
+  TypeUnion,
+  TypeFuncion
+
+  TypeNull,
+
+  TypeConst,
+
+  TypeStruct,
+  TypeBool,
+
+};
+
+#endif
